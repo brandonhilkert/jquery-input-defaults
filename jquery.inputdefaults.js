f7196ac1ede9cd9d4dd380c60ffa579902605ab8
Brandon Hilkert <brandonhilkert@gmail.com>
2012-08-23 09:23:33 -0400

    Update to protect against no val().

--- jquery.inputdefaults.js
+++ jquery.inputdefaults.js
@@ -1,26 +1,30 @@
-(function($) {
-  $.fn.inputDefaults = function(options) {
-    var opts;
-    opts = $.extend({}, $.fn.inputDefaults.defaults, options);
-    return this.each(function() {
-      var $el, defaultValue;
-      $el = $(this);
-      defaultValue = $el.data("defaultValue") || opts.value;
-      if ($el.val() === "") {
-        $el.val(defaultValue);
-      }
-      return $el.focus(function() {
-        if ($el.val() === defaultValue) {
-          return $el.val("");
+(function() {
+
+  (function($) {
+    $.fn.inputDefaults = function(options) {
+      var opts;
+      opts = $.extend({}, $.fn.inputDefaults.defaults, options);
+      return this.each(function() {
+        var $el, defaultValue;
+        $el = $(this);
+        defaultValue = $el.data("defaultValue") || opts.value;
+        if (!$el.val()) {
+          $el.val(defaultValue);
         }
-      }).blur(function() {
-        if ($el.val() === "") {
-          return $el.val(defaultValue);
-        }
+        return $el.focus(function() {
+          if ($el.val() === defaultValue) {
+            return $el.val("");
+          }
+        }).blur(function() {
+          if (!$el.val()) {
+            return $el.val(defaultValue);
+          }
+        });
       });
-    });
-  };
-  return $.fn.inputDefaults.defaults = {
-    value: "Please enter a value..."
-  };
-})(jQuery);
+    };
+    return $.fn.inputDefaults.defaults = {
+      value: "Please enter a value..."
+    };
+  })(jQuery);
+
+}).call(this);
